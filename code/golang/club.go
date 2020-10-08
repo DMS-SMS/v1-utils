a3758af0c18fca5de1a665fd9d677845e082dcf6
지농 <jinhong0719@naver.com>
2020-10-08 16:01:15 +0900

    [MODIFY] <code/golang/club.go> 일부 Code 상수 이름 변경

--- code/golang/club.go
+++ code/golang/club.go
@@ -3,8 +3,8 @@ package golang
 const (
 	// CreateNewClub
 	MemberUUIDsNotIncludeLeaderUUID = -1501
-	MemeberUUIDsIncludeNoExistUUID = -1502
-	LeaderUUIDDuplicate = -1503
+	MemberUUIDsIncludeNoExistUUID = -1502
+	ClubLeaderAlreadyExist = -1503
 	ClubNameDuplicate = -1504
 	ClubLocationDuplicate = -1505
 	ClubMemberDuplicate = -1506
