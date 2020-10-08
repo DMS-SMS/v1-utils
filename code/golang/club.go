eefba38bd7b04afdfcb2e6a42dc72f13d2d1b91c
지농 <jinhong0719@naver.com>
2020-10-08 13:03:21 +0900

    [ADD] <code/golang/club.go> CreateNewClub 메서드에서 사용할 Code 상수 추가

--- code/golang/club.go
+++ code/golang/club.go
@@ -1,1 +1,11 @@
 package golang
+
+const (
+	// CreateNewClub
+	MemberUUIDsNotIncludeLeaderUUID = -1501
+	MemeberUUIDsIncludeNoExistUUID = -1502
+	LeaderUUIDDuplicate = -1503
+	ClubNameDuplicate = -1504
+	ClubLocationDuplicate = -1505
+	ClubMemberDuplicate = -1506
+)
