f5c18d655960802b19d7e0c7ecaaacc4a234a579
Minki Son <migsking@naver.com>
2020-12-06 21:13:30 +0900

    Update outing.py

--- code/python/outing.py
+++ code/python/outing.py
@@ -1,16 +1,42 @@
-# Approved Exception
+# Except on Outing Flow
+
 not_approved_by_parents = -2101
-already_approved_by_parents = -2201
-already_approved_by_teacher = -2202
+rejected_by_parents = -2102
+already_confirm_by_parents = -2103
+
+not_approved_by_teacher = -2104
+rejected_by_teacher = -2105
+already_confirm_by_teacher = -2106
+
+not_out = -2107
+already_out = -2108
+
+not_finish_out = -2109
+already_finish_out = -2110
+
+already_confirm_out_by_teacher = -2111
+
+# - Error code -2101 : 학부모가 외출을 아직 승인 하지 않음
+# - Error code - 2102 : 학부모가 외출을 거절함
+# - Error code - 2103 : 학부모가 외출을 이미 확인함
+
+# - Error code - 2104 : 선생이 외출을 아직 승인 하지 않음
+# - Error code - 2105 : 선생이 외출을 거절함
+# - Error code - 2106 : 선생이 외출을 이미 확인함
+
+# - Error code - 2107 : 아직 외출을 하지 않음
+# - Error code - 2108 : 이미 외출함
+
+# - Error code - 2109 : 아직 외출 완료가 되지 않음
+# - Error code - 2110 : 이미 외출이 완료 됨
+
+# - Error code - 2111 : 이미 외출 승인이 완료 됨
 
 # Not Found 
 not_found = -2301
 
 # Outing Exist
 outing_exist = -2401
-
-# Still Out
-still_out = -2501
 
 # Unauthorized
 unauthorized = -2601
